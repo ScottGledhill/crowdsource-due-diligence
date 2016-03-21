bfe0c562189bd03e02124ce6bacecea20495b315
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 15:26:27 +0000

    API test beiber working

--- rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
@@ -1,7 +1,8 @@
 class TwitterController < ApplicationController
 
   def index
-    @apijson = twitter.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
+    p $twitter.methods.sort
+      @apijson = $twitter.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
