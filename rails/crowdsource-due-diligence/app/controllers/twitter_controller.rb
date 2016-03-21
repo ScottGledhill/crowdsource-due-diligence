2bf76bd80160788b0eb0bea80a6c3df93e194ab2
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 14:57:16 +0000

    Twitter gem testing

--- rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
@@ -1,7 +1,8 @@
 class TwitterController < ApplicationController
 
   def index
-    @api = RestClient.get('https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4')
-    @apijson = JSON.parse(@api)
+    @apijson = twitter.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
   end
 end
