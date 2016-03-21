53584f96af2478020f78692f44c2fc7617ae3cf6
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 14:20:47 +0000

    closes #2

--- rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
@@ -1,7 +1,7 @@
 class TwitterController < ApplicationController
 
   def index
-    @api = RestClient.get('https://api.twitter.com/1.1/search/tweets.json')
+    @api = RestClient.get('https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4')
     @apijson = JSON.parse(@api)
   end
 end
