97b53c545347c5e96af3c837573cf8c622abc645
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 13:47:24 +0000

    Twitter route added

--- rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
@@ -1,3 +1,7 @@
 class TwitterController < ApplicationController
-  @api = RestClient.get('https://api.twitter.com/1.1/search/tweets.json')
+
+  def index
+    @api = RestClient.get('https://api.twitter.com/1.1/search/tweets.json')
+    @apijson = JSON.parse(@api)
+  end
 end
