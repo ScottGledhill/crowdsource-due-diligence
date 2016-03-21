1fd8ab55147fdac87a89973a258f904424d60654
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 16:43:17 +0000

    global returned

--- rails/crowdsource-due-diligence/app/models/twitter_api.rb
+++
@@ -1,7 +0,0 @@
-
-  @twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_KEY']
-  config.consumer_secret     = ENV['TWITTER_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS']
-  config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
-end
--- rails/crowdsource-due-diligence/config/initializers/twitter.rb
+++ rails/crowdsource-due-diligence/config/initializers/twitter.rb
@@ -1,5 +1,5 @@
 
-  @twitter = Twitter::REST::Client.new do |config|
+  $twitter = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_KEY']
   config.consumer_secret     = ENV['TWITTER_SECRET']
   config.access_token        = ENV['TWITTER_ACCESS']
