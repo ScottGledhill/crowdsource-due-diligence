9a7daa8a4e031ae9f31940a761e167e131daea77
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 21:28:15 +0000

    application controller updated, global variable removed

--- rails/crowdsource-due-diligence/app/controllers/application_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def twitter_client
+      Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS']
+      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+    end
+  end
 end
