df9eb71325a26b1beab554e73b22b29eebd04934
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-21 16:00:19 +0000

    name changes edited in database

--- rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
@@ -1,8 +1,7 @@
 class TwitterController < ApplicationController
 
   def index
-    p $twitter.methods.sort
-      @apijson = $twitter.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
+      @results = @twitter.search("Leo Allen", result_type: "recent").take(100).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
