5c80bdf0c192f0174ab6934a8540679af7a342e5
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-22 14:13:35 +0000

    twitter api test

---
+++ rails/crowdsource-due-diligence/spec/features/twitter_request_feature_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'Twitter API request' do
+  it 'queries twitter api' do
+    results = twitter_client.search("makers acadedmy", result_type: "recent").take(100).collect do |tweet|
+     "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+    expect(results).to be_an_instance_of(String)
+  end
+end
