1de7eefdb660d9cdcdcd983584fda310c6f23ed9
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-22 11:04:51 +0000

    merge 7 into development

--- rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
+++ rails/crowdsource-due-diligence/app/controllers/twitter_controller.rb
@@ -1,7 +1,7 @@
 class TwitterController < ApplicationController
 
   def index
-      @results = twitter_client.search("makers academy", result_type: "recent").take(100).collect do |tweet|
+      @results = twitter_client.search("makers acadedmy", result_type: "recent").take(100).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
