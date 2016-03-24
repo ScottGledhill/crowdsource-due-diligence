ff4f3f8f5230782ac0104b2803fcc632e227976f
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-24 16:25:39 +0000

    fixed search results bug

--- rails/crowdsource-due-diligence/spec/features/search_feature_spec.rb
+++ rails/crowdsource-due-diligence/spec/features/search_feature_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 feature 'Search', type: :request do
   scenario 'querying the twitter api' do
     search_term = {search_term: 'Nokia'}
+    search_result = {
+     search_term: 'Nokia',
+     positive: 4,
+     neutral: 1,
+     negative: 50
+                 }
     headers = {'CONTENT TYPE' => 'application/json'}
 
     response_body = VCR.use_cassette("twitter_stub") do
