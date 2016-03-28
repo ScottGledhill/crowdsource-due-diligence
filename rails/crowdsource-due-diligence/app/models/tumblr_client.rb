932ebc1bfa20b8b622eeb6e2dcdeadb392d91a59
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-28 13:06:11 +0100

    tumblr_client class created

---
+++ rails/crowdsource-due-diligence/app/models/tumblr_client.rb
@@ -0,0 +1,6 @@
+Tumblr.configure do |config|
+  config.consumer_key = "consumer_key"
+  config.consumer_secret = "consumer_secret"
+  config.oauth_token = "access_token"
+  config.oauth_token_secret = "access_token_secret"
+end
