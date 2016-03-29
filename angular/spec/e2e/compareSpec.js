b8fd42ab08a6c4bf22ebcb0b77c1b74dfe460d76
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-29 12:24:40 +0100

    e2e test wrote for comapring searches

--- angular/spec/e2e/compareSpec.js
+++ angular/spec/e2e/compareSpec.js
@@ -3,22 +3,17 @@ describe("compare two searchterms",function(){
 
   it("submitting a specific searchword",function(){
     browser.get("http://localhost:8000");
-    var searchTerm = element(by.model('searchTerm'));
-    searchTerm.sendKeys('MacBook');
+    $('#compare-link').click();
+    expect(browser.getCurrentUrl()).toContain('/compare');
+    var searchTermOne = element(by.model('comparison-one'));
+    searchTermOne.sendKeys('Iphone 6');
+    var searchTermTwo = element(by.model('comparison-two'));
+    searchTermOne.sendKeys('Iphone 5');
     $('#submit-search').click();
-
-    // The searchbar is cleared after searching
-    expect(searchTerm.getAttribute('value')).toEqual('')
-
-    // The search contains the searhterm
-    var searchTitle = element(by.css('.search-term'));
-    expect(searchTitle.getText()).toEqual('MacBook');
-
-    // Emojis are shown for each search
-    var emojis = element.all(by.css('.emoji'))
-    expect(emojis.count()).toEqual(3);
-
-
+    var searchTitle = element.all(by.css('.search-term')).first();
+    expect(searchTitle.getText()).toEqual('Iphone 6');
+    var searchTitleTwo = element.all(by.css('.search-term')).last();
+    expect(searchTitleTwo.getText()).toEqual('Iphone 5');
 
   });
 
