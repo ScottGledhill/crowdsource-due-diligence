06e41e80f248b7f99ebab1bf3990a86a064f1671
Eilw <eirikwiig@gmail.com>
2016-03-29 15:21:46 +0100

    added the color into ctrl

--- angular/app/js/controllers/compareController.js
+++ angular/app/js/controllers/compareController.js
@@ -13,6 +13,25 @@ self.results = [];
     })
   };
 
+  self.evaluateSearch = function (search) {
+    if( search.positive > 1.5 * search.negative) {
+      return RESULT_TERMS.positive;
+    } else if( search.negative > 1.5 * search.positive) {
+      return RESULT_TERMS.negative;
+    } else {
+      return RESULT_TERMS.neutral;
+    }
+  };
+
+  self.calcBgCol = function (search) {
+    var evaluated = self.evaluateSearch(search);
+    return COLORCHOICE[evaluated];
+  };
+
+
+    var COLORCHOICE = {'SUCKS': 'red', 'DOESN\'T SUCK': 'green', 'MEH': 'yellow'};
+    var RESULT_TERMS = {'positive': 'DOESN\'T SUCK', 'negative': 'SUCKS', 'neutral': 'MEH'};
+
 
 
 }]);
