fa6d2514675833ac880c40024e293e33d405535f
Eilw <eirikwiig@gmail.com>
2016-03-29 15:05:07 +0100

    add a makesearch method

--- angular/app/js/controllers/compareController.js
+++ angular/app/js/controllers/compareController.js
@@ -1,9 +1,17 @@
 doesItSuck.controller('compareController', ['searchFactory', function(searchFactory){
 
 var self = this;
+self.results = [];
 
   self.makeSearch = function(searchTermOne, searchTermTwo){
-    self.searchTerms = [searchTermOne, searchTermTwo];
-    self.getResults = searc
+    self.searchTerms = [{search_term: searchTermOne}, {search_term: searchTermTwo}];
+    self.searchTerms.forEach(function(searchTerm){
+      searchFactory.query(searchTerm).then(function(response){
+        self.results.unshift(response.data);
+      });
+    })
   };
+
+
+
 }]);
--- angular/spec/unit/compareSpec.js
+++ angular/spec/unit/compareSpec.js
@@ -1,21 +1,49 @@
 describe('compareController', function() {
-  var searchFactoryMock, ctrl, MOCK_MESSAGES, MOCK_SEARCH_RESULTS;
+  var searchFactoryMock, ctrl, $q, searchResult, searchTerm, httpBackend;
 
   beforeEach(module('DoesItSuck'));
 
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($rootScope, _$q_, $controller, $httpBackend){
+    scope = $rootScope.$new();
+    $q = _$q_;
+    httpBackend = $httpBackend;
+    var deferred = $q.defer();
+    deferred.resolve({data:'some value'});
+    searchFactoryMock = {query: function(){} };
+    spyOn(searchFactoryMock,'query').and.returnValue( deferred.promise );
+    searchResult = []
+    searchTerm = {search_term: 'Test searchTerm'};
 
-    searchFactoryMock = {getSearchResult: function(){} };
-      ctrl = $controller('comparecController');
+    ctrl = $controller('compareController', {
+        searchFactory: searchFactoryMock
+      });
     }));
 
-  describe('#getCompareResults', function(){
+  describe('#makeSearch', function(){
+    var convertedSearchTerm;
 
-    it('#makeSearch', function(){
+    beforeEach(function(){
       var searchTermOne = 'iphone6';
       var searchTermTwo = 'iphone5';
+      convertedSearchTerm = {search_term: searchTermOne};
       ctrl.makeSearch(searchTermOne, searchTermTwo);
-      expect(ctrl.searchTerms).toContain('iphone6');
+      httpBackend.whenGET('partials/main-search.html').respond({data: 'Success'});
+
+    })
+
+    it('converts the searchterms into the right format', function(){
+      expect(ctrl.searchTerms).toContain(convertedSearchTerm);
     });
+
+    it('calls the searchfactory with each of the searchterms',function(){
+      expect(searchFactoryMock.query).toHaveBeenCalled();
+    })
+
+    it('the return data is stored in results', function(){
+      console.log(ctrl.results)
+      scope.$apply();
+      expect(ctrl.results).toContain('some value')
+    })
+
   });
 });
