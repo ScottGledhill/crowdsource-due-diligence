f7a8e91fabbb868df458cbab3d366c4c05abb777
Eilw <eirikwiig@gmail.com>
2016-03-30 09:47:48 +0100

    updated comparecontroller

--- angular/app/js/controllers/compareController.js
+++ angular/app/js/controllers/compareController.js
@@ -2,15 +2,21 @@ doesItSuck.controller('compareController', ['searchFactory', function(searchFact
 
 var self = this;
 self.results = [];
+self.ready = false;
 
   self.makeSearch = function(searchTermOne, searchTermTwo){
     var comparison = [];
     self.searchTerms = [{search_term: searchTermOne}, {search_term: searchTermTwo}];
-    self.searchTerms.forEach(function(searchTerm){
-      searchFactory.query(searchTerm).then(function(response){
-        response.data.loaded = true;
-        comparison.unshift(response.data);
-      })
+
+    var promises = self.searchTerms.map(function(searchTerm){
+      return searchFactory.query(searchTerm);
+    });
+    promises.forEach(function(promise){
+      promise.then(function(response){
+      comparison.unshift(response.data);})
+      .finally(function(){
+        self.ready = true;
+      });
     });
     self.results.unshift(comparison);
   };
@@ -36,24 +42,22 @@ self.results = [];
     var firstArray = compArray[0].positive / compArray[0].negative;
     var secondArray =  compArray[1].positive / compArray[1].negative;
     if (firstArray > secondArray){
-      compArray[1].outcome = 'DOESN\'T SUCK'
-      compArray[0].outcome = 'SUCKS'
+      compArray[1].outcome = 'DOESN\'T SUCK';
+      compArray[0].outcome = 'SUCKS';
       return compArray[0].search_term + ' Sucks';
     } else {
-      compArray[1].outcome = 'SUCKS'
-      compArray[0].outcome = 'DOESN\'T SUCK'
+      compArray[1].outcome = 'SUCKS';
+      compArray[0].outcome = 'DOESN\'T SUCK';
       return compArray[1].search_term + ' Sucks';
     }
   };
 
   self.calcBgCol2 = function(search){
     return COLORCHOICE[search.outcome];
-  }
+  };
 
 
     var COLORCHOICE = {'SUCKS': 'red', 'DOESN\'T SUCK': 'green', 'MEH': 'yellow'};
     var RESULT_TERMS = {'positive': 'DOESN\'T SUCK', 'negative': 'SUCKS', 'neutral': 'MEH'};
-
-
 
 }]);
