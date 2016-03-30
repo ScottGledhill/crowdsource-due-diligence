5114f0f1dc194e0010efdc1459fe00200e2a72fe
Rufus Raghunath <rufusraghunath@gmail.com>
2016-03-30 13:44:41 +0100

    refactor compare.html, modify compare ctlr to calculate outcome once promises resolve

--- angular/app/js/controllers/compareController.js
+++ angular/app/js/controllers/compareController.js
@@ -4,7 +4,7 @@ var self = this;
 self.results = [];
 self.ready = false;
 
-  self.makeSearch = function(searchTermOne, searchTermTwo){
+self.makeSearch = function(searchTermOne, searchTermTwo){
   var comparison = [];
   var promiseArray = [];
   self.searchTerms = [{search_term: searchTermOne}, {search_term: searchTermTwo}];
@@ -16,49 +16,44 @@ self.ready = false;
   $q.all(promiseArray).then(function(){
     self.ready = true;
     self.results.unshift(comparison);
+    self.outcome(comparison);
   });
-
-
-
 };
 
-  self.evaluateSearch = function (search) {
-    if( search.positive > 1.5 * search.negative) {
-      return RESULT_TERMS.positive;
-    } else if( search.negative > 1.5 * search.positive) {
-      return RESULT_TERMS.negative;
-    } else {
-      return RESULT_TERMS.neutral;
-    }
-  };
+self.evaluateSearch = function (search) {
+  if( search.positive > 1.5 * search.negative) {
+    return RESULT_TERMS.positive;
+  } else if( search.negative > 1.5 * search.positive) {
+    return RESULT_TERMS.negative;
+  } else {
+    return RESULT_TERMS.neutral;
+  }
+};
 
-  self.calcBgCol = function (search) {
-    var evaluated = self.evaluateSearch(search);
-    return COLORCHOICE[evaluated];
-  };
+self.calcBgCol = function (search) {
+  var evaluated = self.evaluateSearch(search);
+  return COLORCHOICE[evaluated];
+};
 
+self.outcome = function(compArray){
+  var firstArray = compArray[0].positive / compArray[0].negative;
+  var secondArray =  compArray[1].positive / compArray[1].negative;
+  if (firstArray > secondArray){
+    compArray[0].outcome = 'DOESN\'T SUCK';
+    compArray[1].outcome = 'SUCKS';
+    return compArray[1].search_term + ' sucks';
+  } else {
+    compArray[0].outcome = 'SUCKS';
+    compArray[1].outcome = 'DOESN\'T SUCK';
+    return compArray[0].search_term + ' sucks';
+  }
+};
 
+self.calcBgCol2 = function(search){
+  return COLORCHOICE[search.outcome];
+};
 
-  self.outcome = function(compArray){
-    var firstArray = compArray[0].positive / compArray[0].negative;
-    var secondArray =  compArray[1].positive / compArray[1].negative;
-    if (firstArray > secondArray){
-      compArray[0].outcome = 'DOESN\'T SUCK';
-      compArray[1].outcome = 'SUCKS';
-      return compArray[1].search_term + ' Sucks';
-    } else {
-      compArray[0].outcome = 'SUCKS';
-      compArray[1].outcome = 'DOESN\'T SUCK';
-      return compArray[0].search_term + ' Sucks';
-    }
-  };
-
-  self.calcBgCol2 = function(search){
-    return COLORCHOICE[search.outcome];
-  };
-
-
-    var COLORCHOICE = {'SUCKS': 'red', 'DOESN\'T SUCK': 'green', 'MEH': 'yellow'};
-    var RESULT_TERMS = {'positive': 'DOESN\'T SUCK', 'negative': 'SUCKS', 'neutral': 'MEH'};
+var COLORCHOICE = {'SUCKS': 'red', 'DOESN\'T SUCK': 'green', 'MEH': 'yellow'};
+var RESULT_TERMS = {'positive': 'DOESN\'T SUCK', 'negative': 'SUCKS', 'neutral': 'MEH'};
 
 }]);
